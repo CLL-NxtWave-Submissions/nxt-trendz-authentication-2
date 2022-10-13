96a3bef92d75afae8972e11f4dd9adaea2d17b50
Surya T N <suryatej.nim@yahoo.com>
2022-10-13 21:00:38 +0000

    Implemented JSX elements in Cart component to render the intended UI

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,1 +1,17 @@
-// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="cart-bg-container">
+      <img
+        className="cart-main-img"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+      />
+    </div>
+  </>
+)
+
+export default Cart
