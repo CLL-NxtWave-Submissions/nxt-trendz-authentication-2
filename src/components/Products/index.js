c70a031e5af7ac500c0c7537ef401b2ab7df5a4a
Surya T N <suryatej.nim@yahoo.com>
2022-10-13 21:17:53 +0000

    Added code in Products component to redirect to '/login' route, if cookie had expired or doesn't exist

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,17 +1,27 @@
+import {Redirect} from 'react-router-dom'
+import Cookie from 'js-cookie'
 import Header from '../Header'
 import './index.css'
 
-const Products = () => (
-  <>
-    <Header />
-    <div className="products-bg-container">
-      <img
-        className="products-main-img"
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
-        alt="products"
-      />
-    </div>
-  </>
-)
+const Products = () => {
+  const jwtTokenValue = Cookie.get('jwtToken')
+
+  if (jwtTokenValue === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="products-bg-container">
+        <img
+          className="products-main-img"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+        />
+      </div>
+    </>
+  )
+}
 
 export default Products
