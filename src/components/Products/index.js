cf7e67a5a8ab7568a714f8425721ad1d1a645705
Surya T N <suryatej.nim@yahoo.com>
2022-10-13 21:02:39 +0000

    Implemented JSX elements in Products component to render the intended UI

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,1 +1,17 @@
-// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-bg-container">
+      <img
+        className="products-main-img"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+      />
+    </div>
+  </>
+)
+
+export default Products
